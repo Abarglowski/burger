7444bd9689d609c3085c8b2dd74ffd5bb3f7fc94
abarglowski <barglowskidrew@gmail.com>
2019-05-10 12:01:13 -0400

    heroku

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,7 +1,7 @@
 var express = require("express");
 
 var router = express.Router();
-
+var path = require("path");
 var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
